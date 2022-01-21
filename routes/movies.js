e1c53847da54b081ba3b998bd38c76028ce256a4
Naoufal Alaa <alaanaoufal2000@gmail.com>
2022-01-21 17:58:37 +0100

    commit

--- routes/movies.js
+++ routes/movies.js
@@ -1,22 +1,12 @@
-var express = require('express');
-var router = express.Router();
-var fs = require('fs')
-/* GET users listing. */
-router.get('/', async function (req, res, next) {
-    const take = parseInt(req.query.take) || 10;
-    const skip = parseInt(req.query.skip) || 0;
-    const movies = await prisma.movies.findMany({
-        skip: skip,
-        take: take
-    })
-    res.send({
-        data: movies,
-        pagination: {
-            count: movies.length, // Total des enregistrements
-            take: take,   // Nombre d'éléments sélectionnés
-            skip: skip   // Décalage à partir duquel on prend les  données
-        }
-    });
+const express = require('express');
+const router = express.Router();
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+ 
+router.get('/', async function(req, res, next) {
+    const movies = await prisma.movies.findMany({take: 10})
+    console.log(movies)
+  res.send(movies);
 });
-
+ 
 module.exports = router;
